825f2fdc053e73d2c6a8ab8b28eeb4a2bcf5903b
Vitor Lacerda <vitorlacerdafaria7@gmail.com>
2023-08-15 17:25:22 -0300

    feat: add person validation

--- Domain/Entities/Person.cs
+++ Domain/Entities/Person.cs
@@ -11,4 +11,31 @@ public class Person : Entity
     public int Document { get; set; }
     public string FantasyName { get; set; }
     public string Address { get; set; }
+    
+    public bool IsValid(out List<string> errorMessages)
+    {
+        errorMessages = new List<string>();
+
+        if (Type == PersonType.LegalPerson)
+        {
+            if (Document.ToString().Length != 14)
+            {
+                errorMessages.Add("Legal person document length must be 14 characters.");
+            }
+        }
+        else
+        {
+            if (Document.ToString().Length != 11)
+            {
+                errorMessages.Add("Natural person document length must be 11 characters.");
+            }
+        }
+
+        if (string.IsNullOrEmpty(Name) || !Name.All(c => char.IsLetter(c) || c == ' '))
+        {
+            errorMessages.Add("Name should only contain letters and spaces.");
+        }
+
+        return errorMessages.Count == 0;
+    }
 }
