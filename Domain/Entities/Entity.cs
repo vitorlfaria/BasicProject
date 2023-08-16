7e9a2363a31ecbbcbbfe34863a3a868b215093a8
Vitor Lacerda <vitorlacerdafaria7@gmail.com>
2023-08-16 17:34:31 -0300

    chore: infra configuration

--- Domain/Entities/Entity.cs
+++ Domain/Entities/Entity.cs
@@ -1,7 +1,10 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace Domain.Entities;
 
 public class Entity
 {
+    [Key]
     public Guid Id { get; set; }
     public DateTime CreatedAt { get; set; }
     public DateTime? UpdatedAt { get; set; }
